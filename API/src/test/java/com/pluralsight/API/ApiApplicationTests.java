56b0361717e2d52c4f1f8e56cd01f2d5ffa68af0
Alexander Leal <computerlanguage5280@gmail.com>
2025-06-18 10:52:56 -0500

    adding home controller

---
+++ API/src/test/java/com/pluralsight/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
