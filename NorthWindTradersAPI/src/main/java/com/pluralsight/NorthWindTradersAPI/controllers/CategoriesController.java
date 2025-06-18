47cc7491aee97d179758d49402621ae14dd10fc9
Alexander Leal <computerlanguage5280@gmail.com>
2025-06-18 13:22:20 -0500

    copying over code from HomeController to controllers to get started. Added contructors and getters/setters for modules

--- NorthWindTradersAPI/src/main/java/com/pluralsight/NorthWindTradersAPI/controllers/CategoriesController.java
+++ NorthWindTradersAPI/src/main/java/com/pluralsight/NorthWindTradersAPI/controllers/CategoriesController.java
@@ -1,4 +1,20 @@
 package com.pluralsight.NorthWindTradersAPI.controllers;
 
-public class CategoriesController {
-}
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+    @RestController
+    public class CategoriesController {
+
+        // this method will respond to http://localhost:8080/
+        @GetMapping(path="/categories")
+        public String index(@RequestParam(defaultValue="Categories") String category
+        ) {
+            return "Welcome to the categories page: " + category + "!";
+        }
+    }
+
+
+
--- NorthWindTradersAPI/src/main/java/com/pluralsight/NorthWindTradersAPI/controllers/ProductsController.java
+++ NorthWindTradersAPI/src/main/java/com/pluralsight/NorthWindTradersAPI/controllers/ProductsController.java
@@ -1,4 +1,20 @@
 package com.pluralsight.NorthWindTradersAPI.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class ProductsController {
+
+    // this method will respond to http://localhost:8080/
+    @GetMapping(path="/products")
+    public String index(@RequestParam(defaultValue="Products") String products
+    ) {
+        return "Welcome to the products page: " + products + "!";
+    }
 }
+
+
+
